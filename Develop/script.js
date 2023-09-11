4849622460267540432f7cc146e033eb3bb1a986
Jared Byrum <jaredbyrum@gmail.com>
2023-09-10 21:58:21 -0400

    fixed bugs with responses, password resets when you press button again

--- Develop/script.js
+++ Develop/script.js
@@ -5,35 +5,48 @@ var strings = {
   numbers: "0123456789",
   symbols: "!@#$%^&*(){}[]<>,."
 }
-
+// global values for functions
 var chars = ' ';
 var password = ' ';
 var lengthRe = 0;
 
 function getResults() {
+  //length prompt
   lengthRe = prompt("How long should your passord be? 8-128 charcters.");
   if (isNaN(lengthRe) || lengthRe < 8 || lengthRe > 128) {
-    alert("Please input a number between 8 and 128. Please try again!");
+    alert("Please input a 'number' between 8 and 128. Please try again!");
     return false;
   } 
-  if (confirm("Should your password use lowercase letters?")) {
+  // character prompts
+  var lowerRe = confirm("Should your password use lowercase letters?");
+  var upperRe = confirm("Should your password use uppercase letters?");
+  var numberRe = confirm("Should your password use numbers");
+  var symbolRe = confirm("Should your password use symbols");
+  //adds to char string
+  if (lowerRe) {
     chars += strings.lower;
-  }  
-  if (confirm("Should your password use uppercase letters?")) {
+  }
+  if (upperRe) {
     chars += strings.upper;
-  } 
-  if (confirm("Should your password use numbers?")) {
+  }
+  if (numberRe) {
     chars += strings.numbers;
-  } 
-  if (confirm("Should your password use special charcters?")) {
+  }
+  if (symbolRe) {
     chars += strings.symbols;
-  } 
-  return true;
+  }
+  //if all false rejects attempt and restarts app
+  if (!lowerRe, !upperRe, !numberRe, !symbolRe) {
+    alert("Please choose at least one character type and try again.");
+    return false;
+  } else {
+    return true;
+  }
 }
     
 
 
-
+// code for charAt found on Stack Overflow
 var generatePassword = function() {
   for (var i = 0; i < lengthRe; i++) {
   random = chars.charAt(Math.floor(Math.random() * chars.length));
@@ -47,12 +60,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //resets password value to nothing everytime you press the button
   password = "";
   var goodPrompts = getResults(); 
   var passwordText = document.querySelector("#password");
   if (goodPrompts) {
-    password = generatePassword();
-    passwordText.value = password;
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
   } else {
     passwordText.value = "";
   }
