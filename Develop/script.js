4e0008980cfaeeee0fce68a918276fa9799112f9
Jared Byrum <jaredbyrum@gmail.com>
2023-09-10 21:11:47 -0400

    generator working, adjusted code cleaned up redundant functions, fixing responses

--- Develop/script.js
+++ Develop/script.js
@@ -8,74 +8,37 @@ var strings = {
 
 var chars = ' ';
 var password = ' ';
-var lengthRe = ' ';
+var lengthRe = 0;
 
-var results = {
-  lowerRe: function() {
-    lower = confirm("Should your password use lowercase letters?");
-    return lower;
-  },
-  upperRe: function() {
-    upper = confirm("Should your password use uppercase letters?");
-    return upper;
-  },
-  numbersRe: function() {
-    numbers =confirm("Should your password use numbers?");
-    return numbers;
-  },
-  symbolsRe: function() {
-    symbols = confirm("Should your password use symbols?");
-    return symbols;
-  }
+function getResults() {
+  lengthRe = prompt("How long should your passord be? 8-128 charcters.");
+  if (isNaN(lengthRe) || lengthRe < 8 || lengthRe > 128) {
+    alert("Please input a number between 8 and 128. Please try again!");
+    return false;
+  } 
+  if (confirm("Should your password use lowercase letters?")) {
+    chars += strings.lower;
+  }  
+  if (confirm("Should your password use uppercase letters?")) {
+    chars += strings.upper;
+  } 
+  if (confirm("Should your password use numbers?")) {
+    chars += strings.numbers;
+  } 
+  if (confirm("Should your password use special charcters?")) {
+    chars += strings.symbols;
+  } 
+  return true;
 }
+    
 
 
 
 var generatePassword = function() {
-  
-  var lengthPrompt = function () {
-    var lengthRe = prompt("How long should your passord be? 8-128 charcters.");
-    if (lengthRe > 128) {
-      alert("Please choose a password length between 8 and 128.");
-      return;
-    } else if (lengthRe < 8) {
-      alert("Please choose a password length between 8 and 128.");
-      return;
-    } else {
-      console.log(lengthRe);
-      return lengthRe;
-    }
+  for (var i = 0; i < lengthRe; i++) {
+  random = chars.charAt(Math.floor(Math.random() * chars.length));
+  password += random;
   }
-  var availableChars = function() {
-    results.lowerRe();
-    results.upperRe();
-    results.numbersRe();
-    results.symbolsRe();
-    if (lower) {
-      chars += strings.lower;
-    } if (upper) {
-      chars += strings.upper;
-    } if (numbers) {
-      chars += strings.numbers;
-    } if (symbols) {
-      chars += strings.symbols;
-    } else {
-      alert("Please choose at lease one character type.");
-      return;
-    }
-    return chars;
-  }
-  var passwordGen = function (chars, lengthRe, password) {
-    for (var i = 0; i < lengthRe; i++) {
-    password += chars.charAt(Math.floor(Math.random() * chars.length));
-    } 
-    return password;
-  }
-  lengthPrompt();
-  availableChars();
-  passwordGen();
-  console.log(chars);
-  console.log(password);
   return password;
 }
 
@@ -83,12 +46,16 @@ var generatePassword = function() {
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword(chars, password) { 
-  generatePassword();
+function writePassword() {
+  password = "";
+  var goodPrompts = getResults(); 
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  if (goodPrompts) {
+    password = generatePassword();
+    passwordText.value = password;
+  } else {
+    passwordText.value = "";
+  }
 }
 
 // Add event listener to generate button
